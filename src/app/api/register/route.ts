e3db981afc9e225a2b9aba91533d953d849a6f9d
burakalkan55 <alkanburak131@gmail.com>
2025-11-19 22:32:16 +0300

    task adding feature added v2

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -29,7 +29,7 @@ export async function POST(req: Request) {
       data: {
         name,
         email,
-        password: hashedPassword,
+        password: hashedPassword,  
       },
       select: { id: true, name: true, email: true },
     });
