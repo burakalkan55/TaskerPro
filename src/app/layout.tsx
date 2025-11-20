bfb820c589ff72be88e8542361a16cb8e091f615
burakalkan55 <alkanburak131@gmail.com>
2025-11-20 16:22:48 +0300

    icon and name added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,21 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
+export const metadata = {
+  title: "TaskerPro",
+  description:
+    "TaskerPro — Collaborate, organize and manage your tasks efficiently across shared workspaces.",
+  icons: {
+    icon: "./favicon.ico",
+  },
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favico.ico" type="image/x-icon" />
+      </head>
       <body className="min-h-screen flex flex-col bg-[#f5f7fa] text-gray-900">
         <Navbar />
         <div className="flex-1">{children}</div>
